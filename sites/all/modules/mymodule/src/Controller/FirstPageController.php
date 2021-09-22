7d479a43d3e29fc3bd8305659998d9cd4339bb05
ihab <ihab>
2021-09-22 15:14:47 +0200

    exemple service

--- sites/all/modules/mymodule/src/Controller/FirstPageController.php
+++ sites/all/modules/mymodule/src/Controller/FirstPageController.php
@@ -3,9 +3,20 @@
 namespace Drupal\mymodule\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-
+use Drupal\mymodule\Service\FirstService;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 class FirstPageController extends ControllerBase {
 
+    private $_firstService;
+
+    public function __construct(FirstService $firstService){
+        $this->_firstService = $firstService;
+    }
+
+    public static function create(ContainerInterface $container)
+    {
+        return new static($container->get('first_service'));
+    }
     public function firstPage(string $name) {
         //Possiblité de gérer les permissions directement dans la partie metier du controlleur
         // if($this->currentUser->hasPermission('our permission')) {
@@ -15,7 +26,15 @@ class FirstPageController extends ControllerBase {
         // }
         return [
             "#markup" => t("Bonjour @name",['@name' => $name])
-        ];        
+        ];  
     }
 
+
+    public function pageWithService() {
+        //$service = new FirstService();
+        $data = $this->_firstService->getDataFromService();
+        return [
+            "#markup" => "Data from service $data"
+        ];
+    }
 }
--- sites/all/modules/mymodule/src/Routing/OurRoutes.php
+++ sites/all/modules/mymodule/src/Routing/OurRoutes.php
@@ -6,18 +6,21 @@ use Symfony\Component\Routing\Route;
 
 class OurRoutes {
     public function routes() {
+        $methods = ['firstPage' => '/mypage/{name}', 'pageWithService' => '/page-service'];
         $routes = [];
         //Logic Metier
-        $defaults = [
-            "_controller" => '\Drupal\mymodule\Controller\FirstPageController::firstPage',
-            '_title' => 'Our first page'
-        ];
-        $requirements = [
-            "_permission" => 'our permission',
-            "name" => '[A-z]+'
-        ];
-        $routes['mymodule.firstpage'] = new Route('/mypage/{name}', $defaults, $requirements);
-
+        foreach($methods as $key=>$value) {
+            $defaults = [
+                "_controller" => "\Drupal\mymodule\Controller\FirstPageController::$key",
+                '_title' => 'page '.$key
+            ];
+            $requirements = [
+                "_permission" => 'our permission',
+                "name" => '[A-z]+'
+            ];
+            $routes["mymodule.$key"] = new Route($value, $defaults, $requirements);
+        }
+        
         return $routes;
     }
 }
