02363d50b3553e5763ed8b3fae8fae262c4446f9
ihab <ihab>
2021-09-23 15:49:45 +0200

    suite database api

--- sites/all/modules/mymodule/src/Controller/ToDoListController.php
+++ sites/all/modules/mymodule/src/Controller/ToDoListController.php
@@ -4,15 +4,44 @@
 namespace Drupal\mymodule\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Connection;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class ToDoListController extends ControllerBase {
+
+    private $_connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->_connection = $connection;
+    }
+
+    public static function create(ContainerInterface $container)
+    {
+        return new static($container->get('database'));
+    }
+
     public function list(?string $message) {
         $response = "Liste todos";
+
+
         if($message != null) {
             $response.= " $message";
         }
+        $this->renderToDo($response);
         return [
             "#markup" => $response
         ];
+    }
+
+    private function renderToDo(&$response) {
+        $query = $this->_connection->select(TODO_TABLE, 't');
+        $query->fields('t',['title', 'content', 'email']);
+        $result = $query->execute();
+        $response = '<div>';
+        foreach($result as $element) {
+            $response .= "<div>Title : ".$element->title."</div>";
+        }
+        $response .= '</div>';
     }
 }
--- sites/all/modules/mymodule/src/Form/ToDoForm.php
+++ sites/all/modules/mymodule/src/Form/ToDoForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\mymodule\Form;
 
+use Drupal\Core\Database\Connection;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\mymodule\Service\ValidatorService;
@@ -10,14 +11,16 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class ToDoForm extends FormBase {
 
     private $_validator;
+    private $_connection;
 
-    public function __construct(ValidatorService $validator)
+    public function __construct(ValidatorService $validator, Connection $connection)
     {
         $this->_validator = $validator;
+        $this->_connection = $connection;
     }
     public static function create(ContainerInterface $container)
     {
-        return new static($container->get('mymodule.validator'));
+        return new static($container->get('mymodule.validator'), $container->get('database'));
     }
     public function getFormId() {
         return 'mymodule.todo_form';
@@ -51,6 +54,13 @@ class ToDoForm extends FormBase {
 
 
     public function submitForm(array &$form, FormStateInterface $form_state)  {
+
+        $query = $this->_connection->insert(TODO_TABLE);
+        $query->fields([
+            "title" => $form_state->getValue('title'),
+            "content" => $form_state->getValue('content'),
+            "email" => $form_state->getValue('email'),
+        ])->execute();
         $form_state->setRedirect('mymodule.list_todo', ['message' => "Todo ajouté"]);
     }
 }
